98fa0058310e9f77792c3227c25a3ef5b3a16cd4
sambd001 <dsambo.sambo@vm.co.mz>
2025-02-15 20:20:36 +0200

    change headless to true

--- src/test_mumzword.py
+++ src/test_mumzword.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 
 def test_mumzword():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
 
         page.goto("https://www.mumzworld.com/en")
